0cee660173718dc02a4938ef07bb2101383ce223
Anicet <anicet@bdesmarest.onmicrosoft.com>
2023-10-01 00:20:13 -0500

    Adde pie chart description label

--- gradebook_gui.py
+++ gradebook_gui.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from tkinter import filedialog
-import datetime;
-import csv;
-import copy;
+import datetime
+import csv
+import copy
 
 #Create a frame 
-m = tk.Tk();
+m = tk.Tk()
 file_read = [] #List to store the list of values from the file read
 #Add text window to my user interface
 def add_text(m):    
@@ -90,8 +90,8 @@ def computeStudentAvg(grades):
         student_grades = [eval (i) for i in student_records]
         #Compute the grades for each student and add it to the list of student average grades. 
         compute_student_average.append((sum(student_grades)/len(student_grades)))
-    compute_student_average.insert(0,'Average');
-    grades.append(compute_student_average);
+    compute_student_average.insert(0,'Average')
+    grades.append(compute_student_average)
     '''for i in range (len(grades)):
         grades[i].append(compute_student_average[i])'''
     
@@ -108,12 +108,12 @@ def computeClassAvg(grades):
     for i in range (len(list_student_grades)):
         element_instance = list_student_grades[i]; # Get each element in the two-dimensional list
         student_grades = [eval (i) for i in element_instance]; # Convert the grades into integers
-        list_average.append(student_grades);
+        list_average.append(student_grades)
     column_average = [sum(sub_list) / len(sub_list) for sub_list in zip(*list_average)];
     column_average.append(sum(column_average)/len(column_average))
     column_average.insert(0, 'Class Average')
     grades.append(column_average)
-    return grades;
+    return grades
 
 
 
@@ -121,39 +121,39 @@ def computeClassAvg(grades):
 
 def getGrade (number):
     if number >=90:
-        return 'A';
+        return 'A'
     elif number <90 and number >=80:
-        return 'B';
+        return 'B'
     elif number <80 and number >=70:
-        return 'C';
+        return 'C'
     elif number <70 and number >=60:
-        return 'D';
+        return 'D'
     elif number < 60:
-        return 'F';
+        return 'F'
 
 
 # This function specifically only calculates and returns the average grade for each student. 
 def computeStudentAvgReturnOnly(Name_and_Grade_of_all_students):
     # Compute the student average
-    element_i_instance = [];
-    compute_student_average_class= [];
+    element_i_instance = []
+    compute_student_average_class= []
     #Get each element of the list
     for i in range (len(Name_and_Grade_of_all_students)):
         # Getting all the student's names and grades 
-        element_i_instance = Name_and_Grade_of_all_students[i];
+        element_i_instance = Name_and_Grade_of_all_students[i]
         student_name = element_i_instance.pop(0); # Retrieve the student's name.
         # Convert the grades into integers
         student_grades = [eval (i) for i in element_i_instance]
         #Compute the grades for each student and add it to the list of student average grades. 
         compute_student_average = [(sum(student_grades)/len(student_grades))]
-        compute_student_average_class.append(compute_student_average);
-    return compute_student_average_class;
+        compute_student_average_class.append(compute_student_average)
+    return compute_student_average_class
 
 
  
 
 def compute_letter_grade(grades):
-    compute_grade_average_class = [];
+    compute_grade_average_class = []
     list_of_grade_letters = []
     # Get the average grade for each student
     list_student_grades_new = copy.deepcopy(grades)
@@ -166,7 +166,7 @@ def compute_letter_grade(grades):
         for i in item: 
             list_of_grade_letters.append(getGrade(i)) # Add each grade let'ter
     #list_of_grade_letters.insert(0, 'Letter Grade') #Insert the expression 'Letter Grade' for display purposes
-    grades.append (list_of_grade_letters)#Add the letter grades to the original container
+    #grades.append (list_of_grade_letters)#Add the letter grades to the original container
     '''for i in range (len(list_of_grade_letters)):
         grades[i].append(list_of_grade_letters[i])'''
     return list_of_grade_letters
@@ -231,8 +231,18 @@ def createGraph(file):
     for val,col in zip(PieV,colV):    
         canvas.create_arc(coord,start=st,extent = val*3.6,fill=col,outline=col)
         st = st + val*3.6
-    canvas.create_rectangle((350, 350), (450, 150))
-    
+    canvas.create_rectangle((550, 50), (675, 175))
+    #Additional the color description and triangles.
+    canvas.create_rectangle((560, 60), (570, 70), fill='Red')
+    label = canvas.create_text(620, 65, text="Percentage of A" )
+    canvas.create_rectangle((560, 80), (570, 90), fill='Aqua')
+    label = canvas.create_text(620, 85, text="Percentage of B" )
+    canvas.create_rectangle((560, 100), (570, 110), fill='Green')
+    label = canvas.create_text(620, 105, text="Percentage of C" )
+    canvas.create_rectangle((560, 120), (570, 130), fill='Blue')
+    label = canvas.create_text(620, 125, text="Percentage of D" )
+    canvas.create_rectangle((560, 140), (570, 150), fill='Yellow')
+    label = canvas.create_text(620, 145, text="Percentage of F" )
     
 
 def main():
